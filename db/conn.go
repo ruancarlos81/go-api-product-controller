eb900da67e4705ff1bac5dde42a8ebc97b6f8595
Ruan Carlos <93678842+ruancarlos81@users.noreply.github.com>
2024-12-28 14:42:33 -0300

    melhoria

--- db/conn.go
+++ db/conn.go
@@ -2,15 +2,34 @@ package db
 
 import (
 	"database/sql"
+	"fmt"
 
 	_ "github.com/lib/pq"
 )
 
+const (
+	host     = "go_db"
+	port     = 5432
+	user     = "postgres"
+	password = "1234"
+	dbname   = "postgres"
+)
+
 func ConnectDB() (*sql.DB, error) {
-	connStr := "postgres://postgres:1234@go_db:5432/postgres?sslmode=disable"
-	db, err := sql.Open("postgres", connStr) // Usa o driver pq para "postgres"
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		return nil, err
+		panic(err)
 	}
+
+	err = db.Ping()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("Connected to " + dbname)
+
 	return db, nil
 }
